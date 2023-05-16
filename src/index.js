78bfa6c843a7ee189931f5fcb41158369151a64f
eaybgui <eelliieess@gmail.com>
2023-05-16 14:03:57 +0200

    First commit, requests added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.use(cors())
+app.use(express.json())
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+todos = [{
+    "id": 1,
+    "content": "Learn React",
+    "days": "daily",
+    "done": true
+}]
+
+app.get('/api/todos', (request, response) => {
+    response.json(todos)
+})
+
+app.get('/api/todos/:id', (request, response) => {
+    const id = request.params.id
+    const todo = todos.find(todo => todo.id == id)
+
+    if (todo) {
+        response.json(todo)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/todos/:id', (request, response) => {
+    const id = request.params.id
+    todos = todos.filter(todo => todo.id != id)
+    response.status(204).end()
+})
+
+app.post('/api/todos', (request, response) => {
+    const todo = request.body
+
+    const ids = todos.map(todo => todo.id)
+    const maxId = Math.max(...ids)
+
+    const newTodo = {
+        id: maxId + 1,
+        content: todo.todo,
+        days: todo.days,
+        done: false,
+    }
+
+    todos = [...todos, newTodo]
+    console.log(newTodo)
+    response.json(newTodo)
+})
+
+app.put('/api/todos', (request, response) => {
+    const todoId = request.body.id
+
+    //map that return either the same todo or the todo with the todo.done attribute changed
+    todos = todos.map(todo => todo.id != todoId ? todo : { id: todo.id, todo: todo.todo, days: todo.days, done: !todo.done })
+
+    response.json(todoId)
+})
+
+app.use((request, response) => {
+    console.log(request.path)
+    response.status(404).json({
+        error: 'Not found'
+    })
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
